9f9d0014403aaf46dbfbfc5e0eacb61338158acd
Drj2001 <107933233+Darshanrgowda54@users.noreply.github.com>
2025-05-04 21:33:36 +0530

    HeaderFile

---
+++ HeaderFile/EndBrace.h
@@ -0,0 +1,10 @@
+#ifndef ENDBRACE_H
+#define ENDBRACE_H
+
+// int add(int a ,int b)
+// {
+//     return a+b;
+// }
+
+}
+#endif // ENDBRACE_H
---
+++ HeaderFile/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// #include "EndBrace.h"
+
+using namespace std;
+
+int main()
+{
+    // int result = add(5,10);
+    // cout << result << endl;
+    cout << "Hello World!" << endl;
+
+    return 0;
+#include "EndBrace.h"
