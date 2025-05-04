c47e9736570bcf5c225a43f789a102f6bd0840b4
Drj2001 <107933233+Darshanrgowda54@users.noreply.github.com>
2025-05-04 21:31:37 +0530

    Variables

---
+++ Variables/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // char , short, int , long long ,long ,float, double, bool
+
+    int variable = 10;
+    cout << variable<< endl;
+
+    variable = 20;
+    cout << variable<< endl;
+
+    bool value = true;
+    cout << sizeof(value)<< endl;
+    cout << sizeof(double)<< endl;
+    return 0;
+}
