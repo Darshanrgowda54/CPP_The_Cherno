4a6f762301570af2ee0a3eb7585e944c0f8d9ecb
Drj2001 <107933233+Darshanrgowda54@users.noreply.github.com>
2025-05-04 21:37:30 +0530

    Loops

---
+++ Loops/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+
+/// FOR LOOP
+///
+// int main()
+// {
+//     // for(int i = 0; i < 5; i++)
+//     // {
+//     //     cout << "Hello World!" << endl;
+//     // }
+
+//     int i =0;
+//     bool condition = true;
+//     for(; condition;)
+//     {
+//         cout << "Hello World!" << endl;
+//         i++;
+//         if(!(i<5))
+//         condition = false;
+//     }
+//     return 0;
+// }
+
+
+
+/// WHILE LOOP
+
+int main()
+{
+    int i = 0;
+    while(i<5)
+    {
+        cout << "Hello World!" << endl;
+        i++;
+    }
+    return 0;
+}
+
+
+
+/// DO WHILE LOOP
+
+// int main()
+// {
+//     do
+//     {
+//         cout << "Hello World!" << endl;
+//     }
+//     while(0);
+
+//     return 0;
+// }
