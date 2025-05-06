117f86d3eece799ad07f2a289727261383fa38e4
Drj2001 <107933233+Darshanrgowda54@users.noreply.github.com>
2025-05-06 09:58:13 +0530

    Enums

---
+++ Enums/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+enum example
+{
+    A,B,C
+};
+using namespace std;
+
+int main()
+{
+    example value = B;
+    if(value == 1)
+    {
+        cout << "Hello World!" << endl;
+    }
+    return 0;
+}
